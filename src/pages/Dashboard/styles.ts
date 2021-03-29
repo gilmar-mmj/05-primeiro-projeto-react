6f329e9e461f6eb34760d75ee7aeb722129be86e
gilmar-mmj <gilmar.mmj@outlook.com>
2021-03-29 16:10:38 -0300

    dealing with errors

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,7 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
 
-// Template literals = ``
+interface FormProps {
+  hasError: boolean;
+}
+
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
@@ -11,7 +14,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -24,6 +27,14 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c33030;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -44,6 +55,12 @@ export const Form = styled.form`
       background: ${shade(0.2, '#04d361')};
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  margin-top: 8px;
+  color: #c33030;
 `;
 
 export const Repositories = styled.div`
